59db15d68be300a459c8e090876008436e6977e6
鲨鱼辣椒 <monishine_code@163.com>
2025-05-12 17:30:56 +0800

    feat: 统一响应格式

--- app/api/endpoints/douyin.py
+++ app/api/endpoints/douyin.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Response
+from fastapi.responses import JSONResponse
 import re
 import requests
 from pydantic import BaseModel
@@ -7,8 +8,13 @@ from pydantic import BaseModel
 router = APIRouter(tags=["douyin"])
 
 
+"""
+ 2.82 wsr:/ Happy birthday to Kobe.%篮球 %曼巴精神 %科比生日  https://v.douyin.com/d8LpxMQ/ 复制佌鏈接，da鐦Dou音搜索，直接观看視频！
+"""
+
+
 class DouyinVideoParams(BaseModel):
-    share_content: str
+    share_content: str = None
 
 
 # 提取 URL 链接
@@ -33,12 +39,23 @@ def get_redirect_url(url):
 
 
 @router.post("/douyin")
-def get_no_watermark_video_url(videoParams: Body(DouyinVideoParams)):
+# def get_no_watermark_video_url(form_data: DouyinVideoParams = None):
+def get_no_watermark_video_url(form_data: DouyinVideoParams = None):
     # 使用正则表达式匹配 URL
     # urls = re.findall(r"https?://\S+", share_url)
+    if not form_data or not form_data.share_content:
+        return JSONResponse(
+            content={
+                "code": 400,
+                "message": "缺少参数",
+                "data": None,
+            }
+        )
+
+    urls = extract_urls(form_data.share_content)
 
     print("提取到的 URL 链接:")
     for url in urls:
         print(url)
 
-    return urls
+    return JSONResponse(content={"code": 200, "message": "success", "data": urls})
