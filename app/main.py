14a7b6ce2c76703f662da9886079d61d06ace6d0
鲨鱼辣椒 <monishine_code@163.com>
2025-07-01 14:04:41 +0800

    feat: cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
-from app.api.main import api_router
+from app.router.main import api_router
 from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3301",
+    "http://192.168.1.191:3301",
+]
 
 
 def generate_unique_id(route: APIRoute):
@@ -14,5 +20,13 @@ app = FastAPI(
     generate_unique_id_function=generate_unique_id,
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    # allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    # expose_headers=["*"],
+)
 
 app.include_router(api_router, prefix=settings.SERVICE_API_PREFIX)
